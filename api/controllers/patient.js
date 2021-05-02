53577119e51522b5caa9a79366cd73302b51bd9e
esteban pereira <starwarseste@hotmail.fr>
2021-05-02 15:51:28 +0200

    Ajout de la suppresion d'un patient et de la creation d'un patient

--- api/controllers/patient.js
+++ api/controllers/patient.js
@@ -8,7 +8,21 @@ const findOnePatient = (req, res, next) => {
     Patient.findByPk(req.params.id).then(patient => res.json(patient));
 }
 
+const createPatient = (req, res, next) => {
+    Patient.create(req.body).then(patient => res.json(patient));
+}
+
+const deletePatient = (req, res, next) => {
+    Patient.delete({
+        where: {
+            id: req.params.id
+        }
+    }).then(res.json({message : "Patient supprimé avec succès"}));
+}
+
 module.exports = {
     findAllPatients,
     findOnePatient,
+    createPatient,
+    deletePatient,
 }
--- api/routes/patient.js
+++ api/routes/patient.js
@@ -4,7 +4,9 @@ const patientController = require('../controllers/patient');
 
 const router = express.Router();
 
+router.post('/', patientController.createPatient);
 router.get('/:id', patientController.findOnePatient);
+router.delete('//:id', patientController.deletePatient);
 router.use('/', patientController.findAllPatients);
 
 module.exports = router;
